e297c268b57fdf6275f96a2f7b2ecde3e92fb5a1
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2024-09-05 12:50:10 +0200

    deletion of spannzangen now possible

--- src/main/java/com/regofix/spannzangen/controller/ColletController.java
+++ src/main/java/com/regofix/spannzangen/controller/ColletController.java
@@ -36,8 +36,19 @@ public class ColletController {
     @POST
     @Path("/spannzange")
     public Response addCollet(Collet collet) {
-        System.out.println("Received Collet: " + collet);  // Add this for debugging
+        System.out.println("Received Collet: " + collet);
         Collet addedCollet = colletService.addCollet(collet);
         return Response.status(Response.Status.CREATED).entity(addedCollet).build();
+    }
+
+    @DELETE
+    @Path("/spannzange/{id}")
+    public Response deleteCollet(@PathParam("id") int id) {
+        boolean isDeleted = colletService.deleteCollet(id);
+        if (isDeleted) {
+            return Response.ok().entity("Collet with ID " + id + " deleted successfully").build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).entity("Collet with ID " + id + " not found").build();
+        }
     }
 }
