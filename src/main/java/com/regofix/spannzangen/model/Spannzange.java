10b86fac151d3b491282978a2c57bc4bb2c0aa55
Christian Gémesi <christian.gemesi@students.fhnw.ch>
2024-09-05 11:35:40 +0200

    created model

---
+++ src/main/java/com/regofix/spannzangen/model/Spannzange.java
@@ -0,0 +1,50 @@
+package com.regofix.spannzangen.model;
+
+public class Spannzange {
+
+    private int id;
+    private int size;
+    private String type;
+    private String articleNumber;
+
+    // Empty constructor for Flexibility to create an object
+    public Spannzange() {}
+
+    public Spannzange(int id, int size, String type, String articleNumber) {
+        this.id = id;
+        this.size = size;
+        this.type = type;
+        this.articleNumber = articleNumber;
+    }
+
+    // Getter and Setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getArticleNumber() {
+        return articleNumber;
+    }
+    public void setArticleNumber(String articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+
+}
